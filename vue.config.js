44c91c3d4533b110b11ef6b4f1e34228a176c88e
瞿文松 <1607186824@qq.com>
2021-06-27 19:48:27 +0800

    '更新'

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    configureWebpack:{
+        resolve:{
+            alias:{
+                'assets':'src/assets',
+                'common':'src/common',
+                'components':'src/components',
+                'network':'src/network',
+                'views':'src/views',
+            }
+        }
+    }
+}
